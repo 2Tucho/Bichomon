0edff48d139bde87b4c403e8c1f7e9ee640c88e0
2Tucho <albergromero@gmail.com>
2024-02-19 17:32:03 +0100

    Primeros ejercicios resueltos

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
 console.log(document.title);
+
+// Ejercicio 1
+document.getElementById("gen-1").innerHTML = "Generasión 1 Pokimon";
+
+// Ejercicio 2
+
+
+// Ejercicio 3
+let URLactual = window.location.href;
+console.log(URLactual);
+
+
+//Ejercicio 4
+let URLdomain = window.location.host;
+console.log(URLdomain);
+
+//Ejercicio 5
+let estoEsMain = document.getElementsByTagName("main");
+for (let i = 0; i < estoEsMain.length; i++) {
+    let imagenes = document.getElementsByClassName("src")[i];
+    console.log(imagenes);
+}
